a9978f518030d04aa2be49fcde2357a579c8761c
brettschuenemann <brettschuenemann@gmail.com>
2018-03-12 14:27:37 -0700

    update
    update

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ app.use(express.static('views/assets'));
 
 var jsforce = require('jsforce');
 var conn = new jsforce.Connection({
-  // you can change loginUrl to connect to sandbox or prerelease env.
-  // loginUrl : 'https://test.salesforce.com'
 });
 
 router.get("/",function(req,res){
@@ -19,46 +17,23 @@ router.get("/sfdcinfo", function(req, res){
 
 	conn.login('brett@pw.com', 'Salesforce1wIpd13kf8wDWMfbEMi7CA4bS', function(err, userInfo) {
 	  	if (err) { return console.error(err); }
-	  // Now you can get the access token and instance URL information.
-	  // Save them to establish connection next time.
-		console.log(conn.accessToken);
-		console.log(conn.instanceUrl);
-
-
 		var records = [];
 		var custEmail = req.query.email;
 
 		console.log("Customer Email: " + custEmail);
 
-		var query = conn.query("SELECT Id, Name FROM Contact WHERE email =" + "'" + custEmail + "'")
-		  .on("record", function(record) {
-		    records.push(record);
-		  })
-		  .on("end", function() {
-		  		console.log('IN END');
-		  		var r = [];
-			    var q = conn.query("SELECT Id, CaseNumber, CreatedDate, Status, Subject, ContactId FROM Case WHERE ContactId =" + "'" + records[0].Id + "'")
-					.on("record", function(record) {
-						r.push(record);
-					})
-					.on("end", function() {
-						console.log("total in database : " + q.totalSize);
-						console.log("total fetched : " + q.totalFetched);
-						res.send(r);
-					})
-					.on("error", function(err) {
-						console.error(err);
-					})
-					.run({ autoFetch : true, maxFetch : 4000 });
-
-		  })
-		  .on("error", function(err) {
-		    console.error(err);
-		  })
-		  .run({ autoFetch : true, maxFetch : 4000 });
-
+		conn.sobject("Contact")
+			.find({email : custEmail}, '*')
+			.include("Cases") 
+				.select("*")
+				.orderby("CreatedDate", "DESC")
+				.end() 
+			.execute(function(err, r) {
+				if (err) { return console.error(err); }
+				res.send(r);
+		});
 	});
-	});
+});
 
 app.use("/",router);
 
